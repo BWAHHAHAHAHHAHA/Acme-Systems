5c920eb158298c028bcb19f32f6e42e43b61f6df
Sterling Page <sterlingpage9@gmail.com>
2020-06-16 20:58:52 -0400

    added whatfor

--- Acme Systems/src/ver1/HelloWorld.java
+++ Acme Systems/src/ver1/HelloWorld.java
@@ -9,5 +9,10 @@ public class HelloWorld {
 		String Msg = "Hey Now!";
 		System.out.println(Msg);
 	}
+	
+	public static void whatFor() {
+		String Msg = "what for?";
+		System.out.println(Msg);
+	}
 
 }
